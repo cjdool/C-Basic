c0e2b5fcd9e699e5911577441b540918d4c1012a
장주현 <jangjuhyeon@jangjuhyeons-MacBook-Pro>
2018-12-14 08:47:26 -0800

    2nd skeleton

--- lec-03/blackjack.cpp
+++ lec-03/blackjack.cpp
@@ -5,16 +5,35 @@
 
 using namespace std;
 vector<string> cards;
+int players
 
 void swap(int i, int j)
 {
+  auto n = cards[i];
+  cards[i] = cards[j];
+  cards[j] = n;
+}
 
+string draw_card()
+{
+  string str = cards.back();
+  cards.pop_back();
+  return str;
 }
 
 void shuffle_cards()
 {
-  string str;
-  return str;
+  for (int i = 0; i < 1000; i++)
+  {
+    swap(rand() % cards.size(), rand() % cards.size());
+  }
+}
+
+int parse_number(string trump)
+{
+  int 
+
+  
 }
 
 int main(int argc, char **argv)
@@ -24,17 +43,58 @@ int main(int argc, char **argv)
     cout << "Command Line Error!" << endl;
     exit(0);
   }
+  int decks = 4;
   int seed = stoi(argv[1]);
-  int decks = stoi(argv[2]);
+  players = stoi(argv[2]);
 
-  cout << seed << ' ' << decks << endl;
-  int i = 10;
-  string card = to_string(i) + "s";
-  cout << card << endl;
+  if (players < 1)
+  {
+    cout << "Player Must be at least one person" << endl;
+    exit(0);
+  }
 
-  int sum = 0;
-  sum += stoi(card);
-  cout << sum << endl;
+  srand(seed);
+
+  for (int j = 0; j < decks; j++)
+  {
+    for (int i = 0; i <= 13; i++)
+    {
+      string cardnum;
+      if (i == 1)
+        cardnum = "A";
+      else if (i == 11)
+        cardnum == "J";
+      else if (i == 12)
+        cardnum == "Q";
+      else if (i == 13)
+        cardnum == "K";
+      else
+        cardnum = to_string(i);
+      cards.push_back(cardnum + "c");
+      cards.push_back(cardnum + "h");
+      cards.push_back(cardnum + "d");
+      cards.push_back(cardnum + "s");
+    }
+  }
+  shuffle_cards();
+  /*Your Code*/
+
+  vector<string> house_card;
+  vector<vector<string>> players_card;
+
+  house_card.push_back(draw_card());
+  house_card.push_back(draw_card());
+  cout << "House: HIDDEN, " << house_card[1] << endl;
+
+  for (int i = 0; i < players; i++)
+  {
+    players_card[i].push_back(draw_card());
+    players_card[i].push_back(draw_card());
+    int sum = parse_number(player_card[i][0]) + parse_number(player_card[i][1]);
+    cout << "Plater" << i+1 << ": " << player_card[i][0] << ", " << player_card[i][1] << " (" << sum << ")" << endl;
+  }
+
+  
 
   return 0;
 }
